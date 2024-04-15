cafeb95146d9444662ddde73a8967eac39a6f12c
Alfred M. <alpeter91@gmail.com>
2024-04-15 22:32:33 +0300

    Add member profile component

--- src/app/components/MemberProfile.tsx
+++ src/app/components/MemberProfile.tsx
@@ -1,6 +1,13 @@
-const MemberProfile = () => {
+import { UserButton, currentUser, auth } from "@clerk/nextjs"
+
+const MemberProfile = async () => {
+  const user = await currentUser()
+  // const { userId } = auth()
   return (
-    <div>MemberProfile</div>
+    <div className="px-4 flex items-center gap-2">
+      <UserButton afterSignOutUrl="/" />
+      <p>{user?.emailAddresses[0].emailAddress}</p>
+    </div>
   )
 }
 
