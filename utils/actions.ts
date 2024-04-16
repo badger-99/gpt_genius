3df626f2c202d512d32e367f4ec163ed9589a01c
Alfred M. <alpeter91@gmail.com>
2024-04-16 19:52:11 +0300

    Display chat feed

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,31 @@
 'use server';
+import OpenAI from "openai";
 
-export const generateChatResponse = async (chatMessage: string | Array<string>) => {
-  console.log(chatMessage);
-  return 'awesome';
+interface Message {
+  role: string;
+  content: string;
+}
+
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const generateChatResponse = async (chatMessage: Array<any>) => {
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: 'system', content: 'you are a helpful assistant' },
+        ...chatMessage,
+      ],
+      model: 'gpt-3.5-turbo',
+      temperature: 0,
+    });
+    console.log(response.choices[0].message);
+    console.log(response);
+    const message = response.choices[0].message;
+    return message;
+  } catch (error) {
+    return null;
+  }
 };
